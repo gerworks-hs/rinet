18e985809ec3495333ae1a3a1283662d878b98d4
itsgerliz <gerworks.hs@gmail.com>
2024-11-01 12:51:40 +0100

    feat-part(main.rs): Start arg handling with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+const VERSION: &str = "1.0.0";
+
+#[derive(Parser)]
+struct Cli {
+    #[clap(short, long, help = "Target IP address to connect to", required_unless_present = "listening")]
+    target: String,
+    #[clap(short, long, help = "Will rinet run as server?", required = false)]
+    listening: bool,
+    #[clap(short, long, help = "Increase verbosity", required = false)]
+    verbose: bool,
+    #[clap(short, long, help = "Show rinet info", required = false)]
+    info: bool
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli_args = Cli::parse();
 }
